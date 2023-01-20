3498963330159409ef4b89c7e092e9e74d05b702
suraj-singh12 <surajsingh5092@gmail.com>
2023-01-20 23:27:48 +0530

    added cors configuration

--- app.js
+++ app.js
@@ -27,6 +27,25 @@ function auth(key) {
     return false;
 }
 
+//Cors Configuration - Start
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+  //Cors Configuration - End
+  
+
 app.get('/', (req, res) => {
     res.send('Express Server Default');
 });
